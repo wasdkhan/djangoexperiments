cd3b03b2c12bcbfe4e6890675a8c5e6825fa1478
wmkn <waseemkhan96@outlook.com>
2014-07-10 10:45:53 -0400

    edition 0.1

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.mail import send_mail
+from django.template import RequestContext
+from django.core.context_processors import csrf
 
 from contact.forms import NameForm, ContactForm
 
@@ -8,22 +10,21 @@ def get_contact(request):
 	if request.method == 'POST':
 		form = ContactForm(request.POST)
 		if form.is_valid():
-			subject = form.cleaned_data['subject']
-			message = form.cleaned_data['message']
-			sender = form.cleaned_data['sender']
-			cc_myself = form.cleaned_data['cc_myself']
+		    subject = form.cleaned_data['subject']
+		    message = form.cleaned_data['message']
+		    sender = form.cleaned_data['sender']
+		    cc_myself = form.cleaned_data['cc_myself']
 
-			recipients = ['Bobbyjoe500@gmail.com']
+		    recipients = ['mark.natividad@gmail.com']
+		    if cc_myself:
+		        recipients.append(sender)
 
-			if cc_myself: 
-				recipient.append(sender)
-
-			send_mail(subject, message, sender, recipients)
-			return HttpResponseRedirect('thanks/')
+		    send_mail(subject, message, sender, recipients)
+		    return HttpResponseRedirect('thanks/')
 	else:
 		form = ContactForm()
 		
-	return render(request, 'contact/index.html', {'form' : form})
+	return render_to_response("contact/index.html",  {'form': form,  }, context_instance = RequestContext(request))
 
 def thanks(request):
     return HttpResponse("Thank you for sending your message. Your feedback is important to us.")
